c2bd6015ff6306a4f783cac92bf80280c210b16e
Georgi Georgiev <ggfinz@gmail.com>
2021-03-24 15:28:10 +0200

    Add ViewModel

--- SimpleAuthExampleUWP/ViewModel/MainPageViewModel.cs
+++ SimpleAuthExampleUWP/ViewModel/MainPageViewModel.cs
@@ -1,13 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.ComponentModel;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SimpleAuthExampleUWP.ViewModel
 {
-    public class MainPageViewModel
+    public class MainPageViewModel : INotifyPropertyChanged
     {
+        private string _username;
+        private string _password;
+        private string _loginNotification;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Username
+        {
+            get => _username;
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public string LoginNotification
+        {
+            get => _loginNotification;
+            set
+            {
+                _loginNotification = value;
+                OnPropertyChanged(nameof(LoginNotification));
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
