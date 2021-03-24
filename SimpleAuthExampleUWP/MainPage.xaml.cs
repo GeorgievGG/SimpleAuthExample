34178a333822d5698b1a579632ef3c45ccbb0700
Georgi Georgiev <ggfinz@gmail.com>
2021-03-24 15:33:02 +0200

    Add ViewModel property

--- SimpleAuthExampleUWP/MainPage.xaml.cs
+++ SimpleAuthExampleUWP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using SimpleAuthExampleUWP.ViewModel;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -9,9 +10,15 @@ namespace SimpleAuthExampleUWP
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
         public MainPage()
         {
             this.InitializeComponent();
+
+            this.ViewModel = new MainPageViewModel();
+            this.DataContext = this.ViewModel;
         }
+
+        public MainPageViewModel ViewModel { get; }
     }
 }
